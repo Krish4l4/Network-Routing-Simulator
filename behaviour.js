55d58e00de07ca3e1943cd01b7e6b31e46b2c432
Aakash Dutta Subedi <aakashsubedi80@gmail.com>
2024-07-30 19:29:53 +0545

    Added a node when clicked on 'Add Node' Button

--- behaviour.js
+++ behaviour.js
@@ -1,8 +1,29 @@
-function setup(){
-    let canvas= createCanvas(720,480);
-    canvas.parent("canvasContainer");
+let nodes = [];
+let edges = [];
 
+function setup() {
+  createCanvas(720, 480);
+
+  // Add Event listener to attach event handler to specified element
+  document.getElementById("addNodeBtn").addEventListener("click", addNode);
 }
-function draw(){
-    background(230);
+
+function draw() {
+  background(230);
+
+  // iteratively draw nodes
+  for (let i = 0; i < nodes.length; i++) {
+    stroke(0);
+    strokeWeight(2);
+    fill(0);
+    ellipse(nodes[i][0], nodes[i][1], 50, 50);
+    fill(255);
+    text(i, nodes[i][0], nodes[i][1]);
+  }
+}
+
+function addNode() {
+  // push item into array: nodes
+  nodes.push([50, 50]);
+  console.log(nodes);
 }
